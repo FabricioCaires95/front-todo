95b3575a704a66797e3f697a95fa2ad9fdec066a
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-09-11 20:19:22 -0300

    componente para renderizar tasks finalizadas

---
+++ src/app/components/finalizados/finalizados.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Todo } from 'src/app/models/todo';
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'app-finalizados',
+  templateUrl: './finalizados.component.html',
+  styleUrls: ['./finalizados.component.css']
+})
+export class FinalizadosComponent implements OnInit {
+
+  closeTodoList: Todo[] = [];
+
+  constructor(private service: TodoService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service
+        .findAll()
+        .subscribe((response)=> {          
+          this.closeTodoList = response.filter(todo => !todo.status);
+        });
+  }
+
+  backToOpenTodo(): void {
+    this.router.navigate([''])
+  }
+
+}
